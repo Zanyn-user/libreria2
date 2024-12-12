f86049c1cd874f5ddb2eea36fe36d202f8e97a8c
Zanyn-user <zanynuser@gmail.com>
2024-12-12 15:04:12 -0500

    primer comentario

---
+++ src/main/java/ec/edu/espoch/prueba2/Prueba2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.prueba2;
+
+/**
+ *
+ * @author SO-LAB1-PC22
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
